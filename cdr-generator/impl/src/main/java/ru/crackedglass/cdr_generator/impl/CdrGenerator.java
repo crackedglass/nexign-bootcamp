ddccdfae9634a2b35fa162e491b9ebb0f803ef41
monrois <bunin.basil@yandex.ru>
2025-04-26 19:29:58 +0700

    Create blank cdr-generator service

---
+++ cdr-generator/impl/src/main/java/ru/crackedglass/cdr_generator/impl/CdrGenerator.java
@@ -0,0 +1,11 @@
+package ru.crackedglass.cdr_generator.impl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdrGenerator {
+    public static void main(String[] args) {
+        SpringApplication.run(CdrGenerator.class, args);
+    }
+}
